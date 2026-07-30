37ce778a7adbf40bdd37c299542a2580486b4ef1
Michaela de Bruyn <110990763+Michaela-de-Bruyn@users.noreply.github.com>
2026-07-30 18:51:07 +0200

    Amend create database code

--- database/database_creation.sql
+++ database/database_creation.sql
@@ -1,34 +1,4 @@
 --Create the Database
 CREATE DATABASE [urban_basket]
 
---Create the Table 
-CREATE TABLE [dbo].[urban_basket_sales_data](
-	[FirstName] [nvarchar](50) NOT NULL,
-	[LastName] [nvarchar](50) NOT NULL,
-	[Email] [nvarchar](50) NOT NULL,
-	[ContactNumber] [nvarchar](20) NULL,
-	[LoyaltyMember] [nvarchar](50) NOT NULL,
-	[City] [nvarchar](50) NOT NULL,
-	[Province] [nvarchar](50) NOT NULL,
-	[Store] [nvarchar](50) NOT NULL,
-	[ProductName] [nvarchar](50) NOT NULL,
-	[ProductID] [nvarchar](50) NOT NULL,
-	[Category] [nvarchar](50) NOT NULL,
-	[Supplier] [nvarchar](50) NOT NULL,
-	[SaleID] [nvarchar](50) NOT NULL,
-	[OrderDate] [datetime2](7) NOT NULL,
-	[CustomerID] [nvarchar](50) NOT NULL,
-	[UnitPrice] [decimal](10, 2) NULL,
-	[Quantity] [int] NULL,
-	[DiscountPercent] [decimal](5, 2) NULL,
-	[TotalAmount] [decimal](10, 2) NULL,
-	[PaymentMethod] [nvarchar](50) NOT NULL,
-	[PaymentStatus] [nvarchar](50) NOT NULL,
-	[SalesChannel] [nvarchar](50) NOT NULL,
-	[SalesPerson] [nvarchar](50) NOT NULL,
-	[StockLevel] [int] NULL,
-	[Promotion] [nvarchar](50) NULL,
-	[OrderStatus] [nvarchar](50) NOT NULL
-) ON [PRIMARY]
---Alternatively I imported my csv as a flat file instead of manually creating the database.
 
